d683fa76dac277d21afc82bc1df0855c200200be
tenzija <48459321+tenzija@users.noreply.github.com>
2022-09-30 14:34:21 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["Major Mono Display", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "brand-purple-dark" : "var(--clr-purple-dark)",
+        "brand-purple-mid" : "var(--clr-purple-mid)",
+        "brand-purple-light" : "var(--clr-purple-light)",
+        "brand-blue-mid" : "var(--clr-blue-mid)",
+        "brand-blue-light" : "var(--clr-blue-light)",
+        "brand-light" : "var(--clr-light)",
+      }
+    },
+  },
+  plugins: [],
+}
