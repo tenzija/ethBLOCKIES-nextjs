1bb675adc07cc3c8b4afb55c12b6d3949a73145d
tenzija <48459321+tenzija@users.noreply.github.com>
2022-10-04 15:00:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <title>{config.title}</title>
         <meta name="description" content={config.description} />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/2.png" />
       </Head>
 
       <header
